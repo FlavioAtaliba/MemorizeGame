befcbe52ac60bfc053819ec741813de0523b4a1c
FlavioAtaliba <flavioatalibajr@gmail.com>
2024-04-03 16:16:06 -0500

    Initial Commit

---
+++ MemorizeGame/MemorizeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeGameApp.swift
+//  MemorizeGame
+//
+//  Created by user249445 on 4/3/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
