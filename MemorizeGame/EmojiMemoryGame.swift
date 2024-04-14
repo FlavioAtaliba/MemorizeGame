350bb5c924a6dd563be5fe768204ba0bd1bf40ef
FlavioAtaliba <flavioatalibajr@gmail.com>
2024-04-14 15:35:18 -0500

    Create MemoryGame Model and EmojiMemoryGame ViewModel

---
+++ MemorizeGame/EmojiMemoryGame.swift
@@ -0,0 +1,8 @@
+//
+//  EmojiMemoryGame.swift
+//  MemorizeGame
+//
+//  Created by user249445 on 4/14/24.
+//
+
+import Foundation
---
+++ MemorizeGame/MemoryGame.swift
@@ -0,0 +1,8 @@
+//
+//  MemoryGame.swift
+//  MemorizeGame
+//
+//  Created by user249445 on 4/14/24.
+//
+
+import Foundation
